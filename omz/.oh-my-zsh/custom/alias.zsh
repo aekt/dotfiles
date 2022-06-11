2a702dabe6ba4e7e301ca3554e3cdf532fcfa980
Yusan <yusan@oursky.com>
2022-06-11 15:12:38 +0800

    omz: Add config for oh my zsh

---
+++ omz/.oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,4 @@
+alias j='tree -aC -L 1'
+alias jj='tree -aC -L 2'
+alias jjj='tree -aC -L 3'
+alias jjjj='tree -aC -R'
---
+++ omz/.oh-my-zsh/custom/plugins.zsh
@@ -0,0 +1,13 @@
+omz plugin load \
+	asdf \
+	bundler \
+	common-aliases \
+	docker \
+	docker-compose \
+	flutter \
+	fzf \
+	git \
+	helm \
+	kubectl \
+	pip \
+	yarn
