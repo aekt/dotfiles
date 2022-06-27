418ea534fe339edf56396b5d46112026d9afc389
Yusan <yusan@oursky.com>
2022-06-27 22:24:37 +0800

    omz: Show tree with file sizes

--- omz/.oh-my-zsh/custom/alias.zsh
+++ omz/.oh-my-zsh/custom/alias.zsh
@@ -1,4 +1,4 @@
-alias j='tree -aC -L 1'
-alias jj='tree -aC -L 2'
-alias jjj='tree -aC -L 3'
-alias jjjj='tree -aC -R'
+alias j='tree -Cah -L 1'
+alias jj='tree -Cah -L 2'
+alias jjj='tree -Cah -L 3'
+alias jjjj='tree -Cah -R'
