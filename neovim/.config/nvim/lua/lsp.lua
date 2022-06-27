a913cc814642eac795eb197bb0be74cff7858c1a
Yusan <yusan@oursky.com>
2022-06-27 22:25:19 +0800

    neovim: Add language server configs

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -1,7 +1,8 @@
 local lspconfig = require("lspconfig")
 
+lspconfig.bashls.setup({})
+lspconfig.clangd.setup({})
 lspconfig.omnisharp.setup({ cmd = { "omnisharp", "--languageserver" } })
 lspconfig.pyright.setup({})
-lspconfig.tsserver.setup({})
 lspconfig.sumneko_lua.setup({})
-lspconfig.clangd.setup({})
+lspconfig.tsserver.setup({})
